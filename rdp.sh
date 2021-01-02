7f9ce92f62c53ba387878968d54ba4ccb4280505
Phan Hoàng Long <hoanglong@hi.edu.vn>
2021-01-03 00:48:18 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-printf "Installing RDP Be Patience... " >&2
+printf "Tao moi truong va cai dat cac thanh phan chinh..." >&2
 {
-sudo useradd -m ALOK
-sudo adduser ALOK sudo
-echo 'ALOK:8426' | sudo chpasswd
+sudo useradd -m long
+sudo adduser long sudo
+echo 'long:1230' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 sudo add-apt-repository ppa:mozillateam/ppa
@@ -20,17 +20,17 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser ALOK chrome-remote-desktop
+sudo adduser long chrome-remote-desktop
 } &> /dev/null &&
-printf "\nSetup Complete " >&2 ||
-printf "\nError Occured " >&2
+printf "\nCai dat hoan tat " >&2 ||
+printf "\nLoi cai dat " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
+read -p "Dan vao day: " CRP
+su - long -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
-    printf "\n\nUpgrade Completed " >&2
+    printf "\n\nNang cap hoan tat " >&2
 else
-    printf "\n\nError Occured " >&2
+    printf "\n\nLoi nang cap " >&2
 fi
