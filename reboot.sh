ea40188f4748fc9ba5ba91e8979e477eb31459c9
Phan Hoàng Long <hoanglong@hi.edu.vn>
2021-01-03 02:34:39 +0700

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,2 +1,11 @@
+#! /bin/bash
+printf "Installing RDP Be Patience... " >&2
+{
 sudo reboot
+} &> /dev/null &&
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+else
+    printf "\n\nError Occured " >&2
 fi
