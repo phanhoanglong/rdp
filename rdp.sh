e027e361bc2470f6cea94fed54f07c7fc3a55327
Phan Hoàng Long <hoanglong@hi.edu.vn>
2021-01-03 00:59:38 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Dang tao moi truong va cai dat cac thanh phan chinh..." >&2
 {
-sudo useradd -m long
-sudo adduser long sudo
-echo 'long:1230' | sudo chpasswd
+sudo useradd -m longne
+sudo adduser longne sudo
+echo 'longne:1230' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -20,13 +20,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser long chrome-remote-desktop
+sudo adduser longne chrome-remote-desktop
 } &> /dev/null &&
 printf "\nCai dat hoan tat " >&2 ||
 printf "\nLoi cai dat " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Dan vao day: " CRP
-su - long -c """$CRP"""
+su - longne -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
