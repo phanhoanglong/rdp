8fa35b9973e2eaee3f3e81c36407ca2219d72837
long <28734452+phanhoanglong@users.noreply.github.com>
2021-02-15 11:49:32 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -12,10 +12,10 @@ sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
-sudo add-apt-repository ppa:mozillateam/ppa -y
-sudo apt-get install firefox -y
+#sudo apt install --assume-yes xscreensaver
+#sudo systemctl disable lightdm.service
+#sudo add-apt-repository ppa:mozillateam/ppa -y
+#sudo apt-get install firefox -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
