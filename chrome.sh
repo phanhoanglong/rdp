8ec11b2235e4883ae6c27ec619cf375325f74691
long <28734452+phanhoanglong@users.noreply.github.com>
2021-03-02 18:22:48 +0700

    Create chrome.sh

---
+++ chrome.sh
@@ -0,0 +1,14 @@
+sudo adduser long chrome-remote-desktop
+} &> /dev/null &&
+printf "\nXong " >&2 ||
+printf "\nLoi " >&2
+printf '\nDi den https://remotedesktop.google.com/headless  Sao chep ma Debian Linux va\n'
+read -p "Dan vao day: " CRP
+su - long -c """$CRP"""
+printf 'Da hoan tat, kiem tra tai https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "Hoan thanh. " >&2
+else
+    printf "\n\nLoi. " >&2
+fi
